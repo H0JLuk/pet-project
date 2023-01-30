8b4019518e6c68863b11edb8311e3341c92175f4
Sergey Smolkov <damager4425@gmail.com>
2023-01-30 04:20:23 +0300

    refactor: 💡 add index.ts for folder API, small improvements

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { AboutPageLazy } from '@/pages/AboutPage/AboutPage.lazy';
-import { MainPageLazy } from '@/pages/MainPage/MainPage.lazy';
+import { AboutPage } from '@/pages/AboutPage';
+import { MainPage } from '@/pages/MainPage';
 
 import './styles/index.scss';
 
@@ -10,8 +10,8 @@ function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <Routes>
-        <Route path='/' element={<MainPageLazy />} />
-        <Route path='/help-me-pls' element={<AboutPageLazy />} />
+        <Route path='/' element={<MainPage />} />
+        <Route path='/help-me-pls' element={<AboutPage />} />
       </Routes>
     </Suspense>
   );
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,4 +1,4 @@
-declare module '*.scss' {
+declare module '*.modules.scss' {
   interface ClassNames {
     [className: string]: string;
   }
